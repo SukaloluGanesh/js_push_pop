331213e7164727dc8bf29b7418a1865f26a6a9bb
SukaloluGanesh <147176578+SukaloluGanesh@users.noreply.github.com>
2024-11-16 19:51:43 +0530

    push and pop

---
+++ index.js
@@ -0,0 +1,27 @@
+// push and pop
+
+
+
+// let arr = [5, 6, 7, 8, 9, 10];
+
+// let array = {
+//     push: (val) => {
+//         let newArr = [];
+//         for (let i = 0; i < arr.length + 1; i++) {
+//             newArr[i] = arr[i];
+//         }
+//         newArr[arr.length] = val;
+//         arr = newArr;
+//     },
+//     pop: () => {
+//         let newArr = [];
+//         for (let i = 0; i < arr.length - 1; i++) {
+//             newArr[i] = arr[i];
+//         }
+//         arr = newArr;
+//     }
+// }
+// array.push(15);
+// console.log(arr);
+// array.pop();
+// console.log(arr);
